f538a4313a247529e021eb194c0b765a25ddee43
CesarZamudio123 <147220285+CesarZamudio123@users.noreply.github.com>
2023-11-26 20:55:03 -0600

    Update Token.py

--- Token.py
+++ Token.py
@@ -1,31 +1,30 @@
 from tipoToken import tipoToken
 
 class Token():
-    def __init__(self,tipo,lexema,posicion):
-        self.tipo=tipo
-        self.lexema=lexema
-        self.posicion=posicion
-        self.salida=[]
+	def __init__(self,tipo,lexema,posicion):
+		self.tipo=tipo
+		self.lexema=lexema
+		self.posicion=posicion
+		self.salida=[]
 
-    '''def __init__(self,tipo,lexema):
-        self.tipo=tipo
-        self.lexema=lexema
-        self.posicion=0
-        self.salida=[]'''
+	'''def __init__(self,tipo,lexema):
+		self.tipo=tipo
+		self.lexema=lexema
+		self.posicion=0
+		self.salida=[]'''
 
-    def toString(self):
-        return '<'+str(self.tipo)+' '+str(self.lexema)+' '+str(self.posicion)+'>'
+	def toString(self):
+		return '<'+str(self.tipo)+' '+str(self.lexema)+' '+str(self.posicion)+'>'
 
-    def almacenar(self,tipo,lexema,posicion):
-        entrada=['','','']
-        entrada[0]=tipo
-        entrada[1]=lexema
-        entrada[2]=posicion
-        self.salida+=entrada
+	def almacenar(self,tipo,lexema,posicion):
+		entrada=['','','']
+		entrada[0]=tipo
+		entrada[1]=lexema
+		entrada[2]=posicion
+		self.salida+=entrada
 
-    def imprimirEstruct(self):
-        print(self.salida)
+	def imprimirEstruct(self):
+		print(self.salida)
 
-    def obtener()
-        return self.salida
-      
+	def obtener(self):
+		return self.salida
