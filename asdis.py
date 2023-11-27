7168b9be1c8c122818e22ba0cb04353f71cf6847
VillalobosLobo <130194331+VillalobosLobo@users.noreply.github.com>
2023-11-26 20:54:21 -0600

    Create asdis.py

---
+++ asdis.py
@@ -0,0 +1,16 @@
+import interprete as scan
+from Token import Token
+from tipoToken import tipoToken
+
+pila=['']
+salida=''
+
+def analisis(cad):
+		sal=[cad[0]]
+		for i in range(3,len(cad)-1,3):
+			sal.append(cad[i])
+		return sal
+
+#Q(analisis(cad))
+print(analisis(cad))
+print(salida)
