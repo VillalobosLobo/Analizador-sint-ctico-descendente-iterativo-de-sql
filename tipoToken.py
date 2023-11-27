53cc3b44355ac31fb8ae05a3863d8fabd8047bdb
CesarZamudio123 <147220285+CesarZamudio123@users.noreply.github.com>
2023-11-26 19:38:55 -0600

    Update tipoToken.py

--- tipoToken.py
+++ tipoToken.py
@@ -5,3 +5,11 @@ class tipoToken:
   SELECT='SELECT'
   FROM='FROM'
   DISTINCT='DISTINCT'
+
+  #Caracteres
+  COMA='COMA'
+  PUNTO='PUNTO'
+  ASTERISCO='ASTERISCO'
+
+  #Final de cadena 
+  EOF='EOF'
