398b6966dd69dea592d31484f077c7b88d92049c
EtienneGomez <egomezb1701@alumno.ipn.mx>
2023-11-27 00:00:54 -0600

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -3,3 +3,17 @@ import asdis as a
 import sys
 import os
 
+
+if len(sys.argv)>1 and len(sys.argv)<3:
+	if os.path.exists(sys.argv[1]):
+		archivo=open(sys.argv[1],'r')
+		cont=archivo.read()
+		print('')
+		a.asdis(0,cont)
+		print('\n')
+	else:
+		print('\nEl archivo '+sys.argv[1]+' no existe\n')
+else:
+	os.system('clear')
+	while True:
+		a.asdis(1,'')
