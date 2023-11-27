b2fe991b51bddde48c61677a245c5b8c301e45b1
VillalobosLobo <130194331+VillalobosLobo@users.noreply.github.com>
2023-11-26 21:04:17 -0600

    Update asdis.py

--- asdis.py
+++ asdis.py
@@ -5,6 +5,24 @@ from tipoToken import tipoToken
 pila=['']
 salida=''
 
+def D(cad):
+	global salida
+	if cad[0]=='DISTINCT':
+		#P(cad[1:])
+	elif cad[0]=='ASTERISCO':
+		#P(cad)
+	elif cad[0]=='IDENTIFICADOR':
+		#P(cad)
+	elif cad[0]=='FROM':
+		#Qaux(cad)
+	else:
+		salida='Error en D'
+
+def Q(cad):
+	if cad[0]=='SELECT':
+		pila.append(cad[0])
+		D(cad[1:])
+
 def analisis(cad):
 		sal=[cad[0]]
 		for i in range(3,len(cad)-1,3):
